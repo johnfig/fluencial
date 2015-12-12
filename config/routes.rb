b479f5d53d8863b61d6d293134441492b82ec312
John Figueiredo <john@johnfigueiredo.com>
2015-12-11 20:49:09 -0800

    Adding session controller and routes to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'home#index'
-  get 'signup' => 'users#new'
-  resources :users
+  get    'signup'  => 'users#new'
+  post   'signup'  => 'users#create'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 end
