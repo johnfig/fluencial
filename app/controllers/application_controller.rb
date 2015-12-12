6120660cdc3e88b3af37a5161ee1839d4ff7efc0
John Figueiredo <john@johnfigueiredo.com>
2015-12-12 01:32:37 -0800

    Add basic http authentication on production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_filter :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == 'hello' && password == 'world'
+    end if Rails.env.production?
+  end
 end
